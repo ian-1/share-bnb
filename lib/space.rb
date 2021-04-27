7155f6f2bf7695e8b8ca52f073b5064023841a81
Adam <adam.j.mccor@gmail.com>
2021-04-27 14:58:01 +0100

    'add 3 properties to the class'

--- lib/space.rb
+++ lib/space.rb
@@ -1,4 +1,11 @@
 class Space
+  attr_reader :name, :description, :price_per_night
+
+  def initialize(name:, description:, price_per_night:)
+    @name = name
+    @description = description
+    @price_per_night = price_per_night
+  end
 
   def self.create(name:)
     DatabaseConnection.query("INSERT INTO space (name) VALUES ('#{name}')")
--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -11,4 +11,16 @@ describe Space do
       expect(space_1_from_db).to eq space_1
     end
   end
+
+  context 'new instance' do
+    it 'has 3 properties which are available' do
+      name = "10 makers lane"
+      description = "this is a 1 bedroom apartment"
+      price_per_night = 10
+      new_space = described_class.new(name: name, description: description, price_per_night: price_per_night)
+      expect(new_space.name).to eq name
+      expect(new_space.description).to eq description
+      expect(new_space.price_per_night).to eq price_per_night
+    end
+  end
 end
