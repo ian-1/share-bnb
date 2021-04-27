122c006bd1cfbb9c0d66268325c4acc88b8eb145
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-04-27 15:48:37 +0100

    implementation for selecting records from database

--- lib/space.rb
+++ lib/space.rb
@@ -7,8 +7,13 @@ class Space
     @price_per_night = price_per_night
   end
 
-  def self.create(name:)
-    DatabaseConnection.query("INSERT INTO space (name) VALUES ('#{name}')")
+  def self.create(name:, description:, price_per_night:)
+    DatabaseConnection.query("INSERT INTO space (name, description, price_per_night) VALUES ('#{name}', '#{description}', '#{price_per_night}')")
   end
 
+  def self.all
+    result = DatabaseConnection.query('SELECT * FROM space;')
+    Space.new(name: result[0]['name'], description: result[0]['description'],
+              price_per_night: result[0]['price_per_night'])
+  end
 end
--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -1,26 +1,48 @@
 require 'space'
 
 describe Space do
-
   describe '.create' do
     it 'lets user create a database entry' do
-      space_1 = "space_1"
-      Space.create(name: space_1)
-      result_from_db = DatabaseConnection.query("SELECT name FROM space ").first
-      space_1_from_db = result_from_db['name']
-      expect(space_1_from_db).to eq space_1
+      name = '10 makers lane'
+      description = 'this is a 1 bedroom apartment'
+      price_per_night = '$10.00'
+
+      Space.create(name: name, description: description, price_per_night: price_per_night)
+      result_from_db = DatabaseConnection.query('SELECT * FROM space;').first
+      name_from_db = result_from_db['name']
+      description_from_db = result_from_db['description']
+      price_per_night_from_db = result_from_db['price_per_night']
+
+      expect(name_from_db).to eq name
+      expect(description_from_db).to eq description
+      expect(price_per_night_from_db).to eq price_per_night
     end
   end
 
   context 'new instance' do
     it 'has 3 properties which are available' do
-      name = "10 makers lane"
-      description = "this is a 1 bedroom apartment"
+      name = '10 makers lane'
+      description = 'this is a 1 bedroom apartment'
       price_per_night = 10
       new_space = described_class.new(name: name, description: description, price_per_night: price_per_night)
       expect(new_space.name).to eq name
       expect(new_space.description).to eq description
       expect(new_space.price_per_night).to eq price_per_night
+    end
+  end
+
+  describe '.all' do
+    it 'pulls records from the database' do
+      name = '10 makers lane'
+      description = 'this is a 1 bedroom apartment'
+      price_per_night = '$10.00'
+
+      described_class.create(name: name, description: description, price_per_night: price_per_night)
+      space_from_db = described_class.all
+
+      expect(space_from_db.name).to eq name
+      expect(space_from_db.description).to eq description
+      expect(space_from_db.price_per_night).to eq price_per_night
     end
   end
 end
