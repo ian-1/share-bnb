61970715e33033a0cb6dfff5913743377361fd2e
fizzy-fifs <fkinyemi@gmail.com>
2021-04-27 15:56:27 +0100

    Added background styling

--- app.rb
+++ app.rb
@@ -7,9 +7,24 @@ class MakersBnb < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  enable :sessions
+
   get '/' do
+    p $name
+    p $description
+    p $price_per_night
     erb :index 
   end
 
+  get '/space/new' do
+    erb :'space/new'
+  end
+
+  post '/space' do
+    $name = params[:name]
+    $description = params[:description]
+    $price_per_night = params[:price_per_night]
+    redirect '/'
+  end
   run! if app_file == $PROGRAM_NAME
 end
